f516e8926019938f16b413bfc0a9b21975343a7b
Your Name <you@example.com>
2023-02-26 10:35:10 +0000

    social buttons

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+<div className = 'bg-container'>
+<h1 className = 'heading'>
+    Congratulations
+</h1>
+<div className = 'card-container'>
+    <img src = 'https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png' className = 'image1' />
+    <p className = 'person-name'>Kiran V</p>
+    <p className = 'paragraph'>vishnu institute of computer education</p>
+    <img src = 'https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png' className = 'image1' />
+</div>
+
+    
+</div>
+    // Write your code here.
+  )
+  
+ReactDOM.render(element, document.getElementById('root'))
